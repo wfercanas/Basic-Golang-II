6a1f8013f21b825805337c1e41412c674121a74d
wfercanas <fernando.canas@outlook.com>
2022-01-29 18:09:31 -0500

    add/ employee ex.

--- structs/employee.go
+++ structs/employee.go
@@ -7,6 +7,10 @@ type Employee struct {
 	employeeID int
 }
 
+func ChangeEmployeeID(e Employee, newID int) {
+	e.employeeID = newID
+}
+
 func main() {
 	var nathan Employee
 	fmt.Println(nathan)
@@ -25,4 +29,17 @@ func main() {
 
 	// Accessing a field
 	fmt.Println("my name is", mihalis.FirstName, "and my employee ID is", mihalis.employeeID)
+
+	// To assign is to Copy
+	andy := nathan
+	fmt.Println("Andy:", andy)
+	andy.FirstName = "Andy"
+	andy.employeeID = 122235
+	nathan.FirstName = "Nate"
+	fmt.Println("Andy", andy)
+	fmt.Println("Nathan", nathan)
+
+	ChangeEmployeeID(andy, 999876)
+	fmt.Println("Andy", andy)
+
 }
